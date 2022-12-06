5e2faa5c57f691041b7f95e0716d39f00594c61b
julianbischoff <julibischoff@gmail.com>
2022-12-06 11:04:49 -0300

    final

--- src/conteiners/CartContainer/index.jsx
+++ src/conteiners/CartContainer/index.jsx
@@ -2,17 +2,30 @@ import React from 'react'
 import { useContext } from 'react'
 import CartItem from '../../componentes/CartItem';
 import { Shop } from '../../contexts/Shop'
+import generateOrderObject from '../../service/generateOrdenObject';
 
 
 const CartContainer = () => {
 
-    const {products}= useContext(Shop);
+    const {products, calculateTotal}= useContext(Shop);
+
+    const confirmPurchase = () => {
+        //inventado
+        
+        const nombre = "Julian"
+        const telefono = "3513296894"
+        const mail = "julibischoff@gmail.com"
+
+        const generateOrden = generateOrderObject(nombre, telefono, mail, products, calculateTotal())
+        console.log(generateOrden)
+    }
 
     return (
         <div>
             {products.map(product => {
                 return <CartItem  key={product.id} item={product}/>
             })}
+            <button onClick={confirmPurchase}> Buy Now! </button>
         </div>
     )
 }
